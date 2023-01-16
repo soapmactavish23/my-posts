588997f63a01d172b8738b9aab3bcdb3b42dae33
unknown <ricknogueira1231@gmail.com>
2023-01-16 17:36:17 -0300

    Obtendo um usuário por id

--- src/main/java/com/hkprogrammer/myposts/services/UserService.java
+++ src/main/java/com/hkprogrammer/myposts/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.hkprogrammer.myposts.domain.User;
 import com.hkprogrammer.myposts.repository.UserRepository;
+import com.hkprogrammer.myposts.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return user;
 	}
 	
 	
